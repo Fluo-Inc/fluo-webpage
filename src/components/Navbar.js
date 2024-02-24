317a9106b5595f465f724fa3161a0b44cc366e23
Kaisen Ye <75810017+kaisenye@users.noreply.github.com>
2024-02-24 14:28:20 -0800

    modify home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 // assets
@@ -6,30 +6,52 @@ import logo from '../assets/logo.svg';
 
 // css
 import '../css/Navbar.css';
+import { HiMenuAlt3 } from "react-icons/hi";
+import { IoArrowBackCircleOutline } from "react-icons/io5";
+
 
 const Navbar = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const toggleNavbar = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+            if (window.innerWidth > 1000) {
+                setIsExpanded(false);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className='navbar'>
             <Link to="/" className="navbar-logo">
                 <img src={logo} alt="logo" />
             </Link>
-            <div className="navbar-link">
-                {/* <Link to='/technology' 
-                      className="navbar-link-item">
-                    Features
-                </Link> */}
-                <Link to='/technology' 
-                       className="navbar-link-item">
-                    Technology
-                </Link>
-                <Link to='/team' 
-                      className="navbar-link-item">
-                    Team
-                </Link>
+            <div className="navbar-right">
+                <div className={`navbar-link ${isExpanded || windowWidth > 1000 ? 'expanded' : 'collapsed'}`}
+                     onClick={toggleNavbar}>
+                    <div className="navbar-link-item">Products</div>
+                    <div className="navbar-link-item">Use Cases</div>
+                    <Link to='/technology' className="navbar-link-item">Technology</Link>
+                    <Link to='/team' className="navbar-link-item">Team</Link>
+                </div>
+                <Link to="/book-demo" className="navbar-contact">Book a Demo</Link>
+                <div className={`navbar-toggler ${isExpanded || windowWidth > 1000 ? 'expanded' : 'collapsed'}`} onClick={toggleNavbar}>
+                    {/* Icon or text to indicate menu expansion */}
+                    {isExpanded ? <IoArrowBackCircleOutline /> : <HiMenuAlt3 />}
+                </div>
             </div>
-            <Link to="/book-demo" className="navbar-contact">
-                Book a Demo
-            </Link>
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
+import { Link } from 'react-router-dom';
 
 // css
 import '../css/Home.css';
@@ -9,137 +10,178 @@ import SignupField from '../components/SignupField';
 import VideoPlayer from '../components/VideoPlayer';
 import Email from '../components/Email';
 
+
+
+const homeTitleOptions = [
+    "Residential Parking",
+    "Guest Parking",
+    "Residents Access",
+    "Gargage Patrol",
+
+];
+
+
 const Home = () => {
 
     // youtube video id
     const previewUrl = 'https://loca-ai.s3.us-west-1.amazonaws.com/preview.mp4';
-    const guestParkingUrl = 'https://loca-ai.s3.us-west-1.amazonaws.com/5.+Guest+Parking.mp4';
-    const securityUrl = 'https://loca-ai.s3.us-west-1.amazonaws.com/6-security-and-control.mp4';
-    const analyticsUrl = 'https://loca-ai.s3.us-west-1.amazonaws.com/4-parking-analytics.mp4';
     const youtubeVideoId = 'dzrXlkte1Aw';
 
+
+    // home title changing animation
+    const [currentText, setCurrentText] = useState('');
+    const [arrayIndex, setArrayIndex] = useState(0);
+    const [charIndex, setCharIndex] = useState(0);
+
+    useEffect(() => {
+        const typingDelay = 30; // Milliseconds to wait before typing the next character
+        const nextTextDelay = 2200; // Milliseconds to wait before switching to the next text
+        let typingTimeout;
+
+        if (charIndex < homeTitleOptions[arrayIndex].length) {
+            typingTimeout = setTimeout(() => {
+                setCurrentText(currentText => currentText + homeTitleOptions[arrayIndex][charIndex]);
+                setCharIndex(charIndex + 1);
+            }, typingDelay);
+        } else {
+            typingTimeout = setTimeout(() => {
+                setCurrentText('');
+                setCharIndex(0);
+                setArrayIndex((arrayIndex + 1) % homeTitleOptions.length);
+            }, nextTextDelay);
+        }
+
+        return () => clearTimeout(typingTimeout);
+    }, [currentText, arrayIndex, charIndex]);
+
+
     // animation
-    const { ref: titleRef, inView: titleInView } = useInView({
-        triggerOnce: true,
-        threshold: 0.5,
-    });
-
-    const { ref: signupRef, inView: signupInView } = useInView({
-        triggerOnce: true,
-        threshold: 0.5,
-    });
-
-    const { ref: featureRef, inView: featureInView } = useInView({
-        triggerOnce: true,
-        threshold: 0.1,
-    });
+    const { ref: imgRef1, inView: imgInView1 } = useInView({ triggerOnce: true, threshold: 1, delay: 200 });
+    const { ref: titleRef1, inView: titleInView1 } = useInView({ triggerOnce: true, threshold: 1, delay: 400 });
+    const { ref: descriptionRef1, inView: descriptionInView1 } = useInView({ triggerOnce: true, threshold: 1, delay: 600 });
+    const { ref: imgRef2, inView: imgInView2 } = useInView({ triggerOnce: true, threshold: 1, delay: 200 });
+    const { ref: titleRef2, inView: titleInView2 } = useInView({ triggerOnce: true, threshold: 1, delay: 400 });
+    const { ref: descriptionRef2, inView: descriptionInView2 } = useInView({ triggerOnce: true, threshold: 1, delay: 600 });
+    const { ref: imgRef3, inView: imgInView3 } = useInView({ triggerOnce: true, threshold: 1, delay: 200 });
+    const { ref: titleRef3, inView: titleInView3 } = useInView({ triggerOnce: true, threshold: 1, delay: 400 });
+    const { ref: descriptionRef3, inView: descriptionInView3 } = useInView({ triggerOnce: true, threshold: 1, delay: 600 });
 
 
 
     return (
         <div className='Home'>
-            {/* title */}
-            <div className={`home-title ${titleInView ? 'bounce-up' : ''}`}
-                 ref={titleRef}>
-                <div className="home-title-bold">
-                    Automate
-                </div>
-                <div className="home-title-regular">
-                    Apartment Parking Management
-                </div>
-                <div className="home-title-description">
-                    Leverages advanced computer vision technology
-                </div>
-            </div>
-
-            {/* signup field */}
-            <div className={`home-sigup-field ${signupInView ? 'bounce-up' : ''}`}
-                 ref={signupRef}>
-                    <SignupField />
-            </div>
-
-            {/* Home Video */}
-            <div className="home-video">
-                <VideoPlayer s3VideoUrl={previewUrl}  youtubeVideoId={youtubeVideoId}/>
-            </div>
-
-            {/* Home Features */}
-            <div className="home-features">
-                <div className="home-features-top">
-                    <div className="home-features-item">
-                        <div className={`home-features-item-title ${featureInView ? 'slide-up' : ''}`}
-                             ref={featureRef}>
-                            Streamline
-                        </div>
-                        <div className={`home-features-item-title ${featureInView ? 'slide-up' : ''}`}
-                             ref={featureRef}>
-                            Guest Parking
-                        </div>
-                        <div className="home-features-item-description">
-                            Easy access for visitors
-                        </div>
-                        <div className="home-features-item-video-container">
-                            <video
-                                className="home-features-item-video"
-                                src={guestParkingUrl}
-                                autoPlay
-                                muted
-                                // loop
-                                playsInline
-                            />
-                        </div>
+            {/* header */}
+            <div className="home-header">
+                {/* title */}
+                <div className='home-title'>
+                    <div className="home-title-bold">
+                        Automate
                     </div>
-                    <div className="home-features-item">
-                        <div className={`home-features-item-title ${featureInView ? 'slide-up' : ''}`}
-                             ref={featureRef}>
-                            Unauthorized
-                        </div>
-                        <div className={`home-features-item-title ${featureInView ? 'slide-up' : ''}`}
-                             ref={featureRef}>
-                            Vehicle Detection
-                        </div>
-                        <div className="home-features-item-description">
-                            Enhanced security with real-time alerts
-                        </div>
-                        <div className="home-features-item-video-container">
-                            <video
-                                className="home-features-item-video"
-                                src={securityUrl}
-                                autoPlay
-                                muted
-                                // loop
-                                playsInline
-                            />
-                        </div>
+                    <div className="home-title-bold">
+                        {currentText}<span className="cursor">|</span>
                     </div>
-
+                    <div className="home-title-description">
+                        Streamline and scale your 
+                        <span className='primary'> residential parking operations </span>
+                        with our AI-powered solution, 
+                        designed to enhance security and improve tenent experience.
+                    </div>
+                    {/* signup field
+                    <div className={`home-sigup-field ${signupInView ? 'bounce-up' : ''}`}
+                        ref={signupRef}>
+                            <SignupField />
+                    </div> */}
+                     <Link to="/book-demo" className="home-contact">
+                        Free Trial
+                    </Link>
                 </div>
-                <div className="home-features-bottom">
-                    <div className="home-features-item">
-                        <div className="home-features-item-title">
-                            Comprehensive Metrics
+
+                {/* Home Video */}
+                <div className="home-video">
+                    <VideoPlayer s3VideoUrl={previewUrl}  youtubeVideoId={youtubeVideoId}/>
+                </div>
+            </div>
+
+            {/* home slogan */}
+            <div className="home-benefits">
+                <div className="home-benefits-title">
+                    Unleash property manager's potential
+                </div>
+                <div className="home-benefits-description">
+                    Fluo takes care your residential parking tasks, 
+                    giving property managers the freedom to focus on what matters most.
+                </div>
+
+                <div className="home-benefits-item">
+                    <div className={`home-benefits-item-img ${imgInView1 ? 'slide-up' : ''}`} 
+                         ref={imgRef1}>
+                        <img src="https://loca-ai.s3.us-west-1.amazonaws.com/svg/home-benefit-1.png"
+                             alt="efficient work" />
+                    </div>
+                    <div className="home-benefits-item-text">
+                        <div className={`home-benefits-item-title ${titleInView1 ? 'slide-up' : ''}`} 
+                             ref={titleRef1}>
+                            A foundation for more efficient work
                         </div>
-                        <div className="home-features-item-description">
-                            Real-time monitoring with user-friendly dashboard
-                        </div>
-                        <div className="home-features-item-video-container">
-                            <video
-                                className="home-features-item-video"
-                                src={analyticsUrl}
-                                autoPlay
-                                muted
-                                // loop
-                                playsInline
-                            />
+                        <div className={`home-benefits-item-description ${descriptionInView1 ? 'slide-up' : ''}`} 
+                             ref={descriptionRef1}>
+                            With your data centralized in one platform, 
+                            there are endless possibilities to transform the 
+                            way your team works by applying automation and 
+                            AI to maximize productivity and values.
                         </div>
                     </div>
                 </div>
+
+
+                <div className="home-benefits-item">
+                    <div className={`home-benefits-item-img ${imgInView2 ? 'slide-up' : ''}`} 
+                         ref={imgRef2}>
+                        <img src="https://loca-ai.s3.us-west-1.amazonaws.com/svg/home-benefit-2.png"
+                             alt="efficient work" />
+                    </div>
+                    <div className="home-benefits-item-text">
+                        <div className={`home-benefits-item-title ${titleInView2 ? 'slide-up' : ''}`} 
+                             ref={titleRef2}>
+                            An unrivaled experience, for everyone
+                        </div>
+                        <div className={`home-benefits-item-description ${descriptionInView2 ? 'slide-up' : ''}`} 
+                             ref={descriptionRef2}>
+                            Intuitive, easy-to-use design translates across devices and teams, 
+                            making every interaction feel effortless for your team, 
+                            your residents, and owners — anytime, and anywhere.
+                        </div>
+                    </div>
+                </div>
+                <div className="home-benefits-item">
+                    <div className={`home-benefits-item-img ${imgInView3 ? 'slide-up' : ''}`} 
+                         ref={imgRef3}>
+                        <img src="https://loca-ai.s3.us-west-1.amazonaws.com/svg/home-benefit-3.png"
+                             alt="efficient work" />
+                    </div>
+                    <div className="home-benefits-item-text">
+                        <div className={`home-benefits-item-title ${titleInView3 ? 'slide-up' : ''}`} 
+                             ref={titleRef3}>
+                            A framework designed for growth and scale
+                        </div>
+                        <div className={`home-benefits-item-description ${descriptionInView3 ? 'slide-up' : ''}`} 
+                             ref={descriptionRef3}>
+                            Easily manage multiple parking sites from a single platform, 
+                            peace of mind for property managers and security staff.
+                        </div>
+                    </div>
+                </div>
+                
             </div>
+
+            
+
+            
 
             {/* Home Email */}
-            <div className="home-email">
+            {/* <div className="home-email">
                 <Email />
-            </div>
+            </div> */}
 
         </div>
     );
